badce3be646bc3df96783d8b6968e41f8ad68b0c
Kelli <kellishelton@gmail.com>
2020-12-12 11:15:10 -0800

    added npm test

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
     ssh
         .connect({
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '54.226.147.35',
+            host: '54.82.53.104',
             username: 'ubuntu',
             privateKey: 'todo-key.pem'
         })
